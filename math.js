fb0320f5755b61cadc023d3ff8443e76c4c828bc
dheena-ts <dheena.ts@mitrahsoft.in>
2026-03-16 13:22:39 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,19 @@
+//add
+function add(a,b){
+    return a+b
+}
+
+
+//subtract
+function subtract(a,b){
+    return a-b
+}
+
+//multiply
+function multiply(a,b){
+    return a*b
+}
+
+console.log(add(10,20))
+console.log(subtract(10,20))
+console.log(multiply(10,20))
