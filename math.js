25ba43688866cce35a407becee019fb5d409ac67
dheena-ts <dheena.ts@mitrahsoft.in>
2026-03-17 11:51:07 +0530

    divide function added

--- math.js
+++ math.js
@@ -15,11 +15,14 @@ function multiply(a,b){
     return a*b
 }
 
+
+//divide
 function divide(a,b){
     return a/b
 }
 
+
 console.log(add(10,20))
 console.log(subtract(10,20))
-console.log(multiply(10,20))
+console.log(multiply(5,10))
 console.log(divide(10,20))
